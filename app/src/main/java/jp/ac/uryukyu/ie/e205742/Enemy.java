8979ae2e760a7f4bacead9bdbc17474067c3295e
e205742 <e205742@ie.u-ryukyu.ac.jp>
2020-12-06 15:42:28 +0900

    レポート４ステップ１

--- app/src/main/java/jp/ac/uryukyu/ie/e205742/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205742/Enemy.java
@@ -8,112 +8,19 @@ package jp.ac.uryukyu.ie.e205742;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Enemy {
-    private String name;
-    private int hitPoint;
-    private int attack;
-    private boolean dead;
+public class Enemy extends LivingThing {
 
-    /**
-     * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name モンスター名
-     * @param maximumHP モンスターのHP
-     * @param attack モンスターの攻撃力
-     */
-    public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    public Enemy(String _name,int _hitPoint,int _attack){
+        super(_name,_hitPoint,_attack);
     }
 
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        if(this.dead==false){
-            int damage = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-        }
-
-
-    }
-
-    /**
-     * 自身へ攻撃されたときのダメージ処理をするメソッド。
-     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
-     * @param damage 受けたダメージ
-     */
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
             dead = true;
             System.out.printf("モンスター%sは倒れた。\n", name);
-        }      
+        }
     }
 
-    /**
-     * フィールド変数(name)に値を代入するsetterメソッド
-     * @param name  enemyの名前
-     */
-    public void setName(String name){
-        this.name=name;
-    }
-    /**
-     * フィールド変数(hitPoint)に値を代入するsetterメソッド
-     * @param hitPoint　enemyのHP
-     */
-    public void setHitpoint(int hitPoint){
-        this.hitPoint=hitPoint;
-    }
-    /**
-     * フィールド変数(attack)に値を代入するsetterメソッド
-     * @param attack　enemyの攻撃力
-     */
-    public void setAttack(int attack){
-        this.attack=attack;
-    }
-    /**
-     * フィールド変数(dead)に値を代入するsetterメソッド
-     * @param dead enemyの生死の状態
-     */
-    public void setDead(boolean dead){
-        this.dead=dead;
-    }
-    
-
-
-    /**
-     * フィールド変数nameにアクセスすgetterメソッド
-     * @return enemyの名前
-     */
-    public String getName(){
-        return this.name;
-    }
-    /**
-     * フィールド変数hitPointにアクセスするgetterメソッド
-     * @return　enemyの現在のHP
-     */
-    public int getHitpoint(){
-        return this.hitPoint;
-    }
-    /**
-     * フィールド変数attackにアクセスするgetterメソッド
-     * @return　enemyの攻撃力
-     */
-    public int getAttack(){
-        return this.attack;
-    }
-    /**
-     * フィールド変数deadにアクセスするgetterメソッド
-     * @return　enemyの生死の状態
-     */
-    public boolean getDead(){
-        return this.dead;
-    }
 
 }
